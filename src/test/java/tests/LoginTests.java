4f5b42509ee75210ac339244bcb1e3c3eb8dffff
urgen-BYD <wolf356700@gmail.com>
2025-02-12 08:59:35 +0200

    tests

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+
 
 public class LoginNegativeTests {
     private WebDriver driver;
